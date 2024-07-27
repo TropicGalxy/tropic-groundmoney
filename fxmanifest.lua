95353eef5fe5e40d5fb427cd2ff0014188794325
TropicGalxy <104592582+TropicGalxy@users.noreply.github.com>
2024-07-27 16:54:42 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Your Name'
-description 'Random Money Spawn Script'
+author 'TropicGalxy'
+description 'a simple but cool way to find money'
 version '1.0.0'
 
 shared_scripts {
